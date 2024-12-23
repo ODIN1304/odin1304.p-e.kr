1586aff17e5a55370bd907d657a4ebca586bb3cd
백인환, Baek Inhwan <57837525+ODIN1304@users.noreply.github.com>
2024-12-23 10:17:23 +0900

    Create welcome.aspx.designer.cs

---
+++ iis/welcome/welcome.aspx.designer.cs
@@ -0,0 +1,13 @@
+namespace SimpleLogin
+{
+    public partial class Welcome
+    {
+        // 사용자의 이름을 표시하기 위한 레이블 컨트롤
+        protected global::System.Web.UI.WebControls.Label lblUsername;
+        /* 
+           lblUsername은 "Welcome.aspx" 파일과 연결된 서버 컨트롤입니다.
+           사용자가 로그인한 후, 세션에서 사용자 이름을 가져와 lblUsername에 표시됩니다.
+           이는 Welcome 페이지에서 사용자 맞춤 메시지를 제공하는 역할을 합니다.
+        */
+    }
+}
