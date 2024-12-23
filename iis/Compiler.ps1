379a34d12167fd8ded6d8e73958c4d420fb7e2ef
백인환, Baek Inhwan <57837525+ODIN1304@users.noreply.github.com>
2024-12-23 11:13:33 +0900

    Create Compiler.ps1

---
+++ iis/Compiler.ps1
@@ -0,0 +1,12 @@
+# Compile MyApp.dll
+
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe 
+    /target:library 
+    /out:
+        [WORKSPACE_FOLDER]\bin\myApp.dll 
+        [WORKSPACE_FOLDER]\login\login.aspx.cs 
+        [WORKSPACE_FOLDER]\login\login.aspx.designer.cs 
+        [WORKSPACE_FOLDER]\welcome\Welcome.aspx.cs 
+        [WORKSPACE_FOLDER]\welcome\Welcome.aspx.designer.cs
+
+# 경로 수정 등 필요함
