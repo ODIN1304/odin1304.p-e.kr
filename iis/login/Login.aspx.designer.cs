e9e953844399db5f7b531430c00cda370a17bcd5
백인환, Baek Inhwan <57837525+ODIN1304@users.noreply.github.com>
2024-12-23 10:15:27 +0900

    Create Login.aspx.designer.cs

---
+++ iis/login/Login.aspx.designer.cs
@@ -0,0 +1,17 @@
+namespace SimpleLogin
+{
+    public partial class Login
+    {
+        // 사용자에게 메시지를 표시하기 위한 레이블 컨트롤
+        protected global::System.Web.UI.WebControls.Label lblMessage;
+
+        // 사용자 이름을 입력받는 텍스트 박스 컨트롤
+        protected global::System.Web.UI.WebControls.TextBox txtUsername;
+
+        // 비밀번호를 입력받는 텍스트 박스 컨트롤
+        protected global::System.Web.UI.WebControls.TextBox txtPassword;
+
+        // 로그인 작업을 트리거하는 버튼 컨트롤
+        protected global::System.Web.UI.WebControls.Button btnLogin;
+    }
+}
